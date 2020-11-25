cdc624e69b03b7215d6238a13b59beeefe32e054
sphilmoon <sphilmoon@gmail.com>
2020-11-25 19:00:48 +0900

    changed orders and modified comments.

--- vowels3.py
+++ vowels3.py
@@ -1,16 +1,18 @@
 # for loops do not use pre-defined variables.
-# it uses the temporarily defined variables within its loop.
+# instead, it uses the temporarily defined variables within its loop.
 # using a list to avoid any duplicates.
-# an interactive mode to find the unique vowels entered by the user. 
 
-vowels = ['a', 'e', 'i', 'o', 'u']
+# This is an interactive mode to find the unique vowels entered by the user.
+
 word = input("Provide a word to search for vowels: ")
-found = []
+vowels = ['a', 'e', 'i', 'o', 'u'] # a list
+found = [] # starting with an empty list.
+
 for letter in word: # checking the word membership with "in".
     if letter in vowels: # any vowels in word?
         if letter not in found: # if these vowels are not in 'found',
-            found.append(letter) # append these vowels to 'found'.
+            found.append(letter) # then append the 'letter' to 'found'.
 for vowel in found: # using a new for loop variable 'vowel'.
     print(vowel) # then print those vowels.
 
-print(found)
+print(found) # also, print the 'found' to list these vowels.
