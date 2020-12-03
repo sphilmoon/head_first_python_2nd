de1876a0ede7bfa50a45d6a08994a54d38bb1ad6
sphilmoon <sphilmoon@gmail.com>
2020-12-03 18:15:01 +0900

    introducing tuples that are immutable.

---
+++ tuples.py
@@ -0,0 +1,10 @@
+# tuples are surrounded by ().
+# tuples are immutable, CAN'T change.
+
+vowels = ('a', 'e', 'i', 'o', 'u') # () indicating a tuple.
+print(vowels)
+print(type(vowels))
+
+# Every string needs a comma b/w the ().
+use_comma_for_tuple = ('python',) # a comma indicates tuple, NOT string.
+print(type(use_comma_for_tuple))
