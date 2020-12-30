b391faf0bf0829b8ece1cc84bbda00d60356a49b
sphilmoon <sphilmoon@gmail.com>
2020-12-30 20:24:32 +0900

    added a few comments.

--- tuples.py
+++ tuples.py
@@ -8,3 +8,14 @@ print(type(vowels))
 # Every string needs a comma b/w the ().
 use_comma_for_tuple = ('python',) # a comma indicates tuple, NOT string.
 print(type(use_comma_for_tuple))
+
+# adding a new variable in existing <class 'tuple'> will cause an error. 
+'''
+vowels[2] = 'W'
+print(vowles)
+
+Traceback (most recent call last):
+  File "tuples.py", line 12, in <module>
+    vowels[2] = 'W'
+TypeError: 'tuple' object does not support item assignment
+''' 
