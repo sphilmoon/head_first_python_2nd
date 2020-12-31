e5e2649526e4a74fbce74630c59b4773b7e8a725
sphilmoon <sphilmoon@gmail.com>
2020-12-31 18:46:26 +0900

    added minor commentaries.

--- vowels7.py
+++ vowels7.py
@@ -1,6 +1,7 @@
-# A set version of vowels3.py
+# A set version of vowels3.py, which is considerably simpler.
 
 word = input("Provide a word to search for vowels: ")
 vowels = set("aeiou") # new set of vowels.
-found = vowels.intersection(set(word))
+found = vowels.intersection(set(word)) # reports any common objects found.
 print(found) # Since I'm using the set, the output will be in a {}.
+
